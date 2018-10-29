031383ea9bbfa546c458ddcaadb94d85bfc11ffa
HarishManen <43129639+HarishManen@users.noreply.github.com>
2018-10-29 12:31:48 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHandler(t *testing.T) {
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body: "Congratulations",
+		Body: "Congratulations MANEN",
 	}
 
 	response, err := Handler(request)
